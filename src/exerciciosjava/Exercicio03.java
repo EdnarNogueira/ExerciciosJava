e715736835d62c75e7a0242f251460a1160c0bce
Edinar Nogueira <edinaredms@gmail.com>
2022-03-03 10:25:53 -0300

    Adicionando exercicio 3

---
+++ src/exerciciosjava/Exercicio03.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosjava;
+
+/**
+ *
+ * @author User
+ */
+public class Exercicio03 {
+    public static void main(String[] args) {
+        
+        /*
+        Escreva um algoritmo que solicite ao usuário 2 valores e 
+        calcule soma, subtração, divisão e multiplicação dos valores informados
+        imprimindo da tela.
+        
+        EX: A soma dos valores informados é de : 
+            O diferença do valores informados é de:
+            A multiplicação do valores informados é de:
+            A divisão do valores informados é de:
+        
+         Obs. podem apresentar os dados da forma que acharem melhor.
+        
+        */
+    }
+    
+}
