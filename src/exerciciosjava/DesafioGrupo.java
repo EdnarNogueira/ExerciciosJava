b79807f3fa81681f8025463bc71bafc5770c9519
Edinar Nogueira <edinaredms@gmail.com>
2022-03-03 20:21:12 -0300

    Resolvendo Desafio grupo

--- src/exerciciosjava/DesafioGrupo.java
+++ src/exerciciosjava/DesafioGrupo.java
@@ -5,11 +5,18 @@
  */
 package exerciciosjava;
 
+import java.util.Scanner;
+
 /**
  *
  * @author User
+ * 
+ *
  */
 public class DesafioGrupo {
+    
+     
+    
     public static void main(String[] args) {
         /*
          FAÇA UM PROGRAMA QUE MOSTRE TODOS OS NUMEROS PRIMOS ENTRE 1 E N
@@ -18,5 +25,31 @@ public class DesafioGrupo {
          OS NUMERO PRIMOS.       
         */
         
+        //=============RESPOSTA====================
+        
+      int primo = 0; // Inicializando a variável primo
+      Scanner teclado = new Scanner(System.in);
+      System.out.println("INFORME O VALOR MAXIMO"); // solicitando o valor maximo do usuário      
+      int n = teclado.nextInt(); // Atribuindo o valor do usuário na variável n
+        
+      for(int i = 1; i <= n ; i++){ // primeiro laço de repetição iniciando em 1 e indo ate o valor de n
+        for(int j = 1; j <= i ; j++){ // segundo laço iniciando em 1 indo ate quando for menor que i
+            if(i % j == 0){ // comparando se o resto da divisão da posição i pela posição j resulta em 0
+                 primo++; // a variável primo e incrementada caso a divisão não tenha sobra
+            }
+        }
+        
+        if(primo == 2){ // testando condição do valor recebido pela variável primo
+            System.out.println("O numero " + i + " é Primo");
+        }
+        
+        primo = 0;
     }
+        
+        
+        
+        
+    }
+    
+    
 }
