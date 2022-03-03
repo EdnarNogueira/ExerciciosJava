510ad80451ff1290832a1ecdb17d70b8fd26ef29
Edinar Nogueira <edinaredms@gmail.com>
2022-03-03 10:01:28 -0300

    Adicionando exercicio 2

---
+++ src/exerciciosjava/Exercicio02.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosjava;
+
+/**
+ *
+ * @author User
+ */
+public class Exercicio02 {
+    public static void main(String[] args) {
+        
+        /* Crie um programa que leia o nome e o salário de um funcionário, mostrando no
+         final uma mensagem.
+         Ex:
+         Nome do Funcionário: Maria do Carmo
+         Salário: 1850,45
+         O funcionário Maria do Carmo tem um salário de R$1850,45 em Junho.
+        */
+        
+        
+    }
+            
+}
