a22db6d8e5d24631003a23d51af776e4fbf384f6
Edinar Nogueira <edinaredms@gmail.com>
2022-03-03 12:29:16 -0300

    Atualizando desafio

--- src/exerciciosjava/DesafioGrupo.java
+++ src/exerciciosjava/DesafioGrupo.java
@@ -10,5 +10,13 @@ package exerciciosjava;
  * @author User
  */
 public class DesafioGrupo {
-    
+    public static void main(String[] args) {
+        /*
+         FAÇA UM PROGRAMA QUE MOSTRE TODOS OS NUMEROS PRIMOS ENTRE 1 E N
+         SENDO N UM NUMERO INTEIRO FORNECIDO PELO USUÁRIO. O PROGRAMA DEVERÁ 
+         MOSTRAR TAMBÉM O NUMERO DE DIVISÕES QUE ELE EXECUTOU PARA ENCONTRAR 
+         OS NUMERO PRIMOS.       
+        */
+        
+    }
 }
