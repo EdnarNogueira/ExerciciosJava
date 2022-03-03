d90ba02324da42c50008c3653c0c0d0ec6d1476d
Edinar Nogueira <edinaredms@gmail.com>
2022-03-03 09:41:54 -0300

    Adicionando exercicio 1

---
+++ src/exerciciosjava/Exercicio01.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosjava;
+
+/**
+ *
+ * @author User
+ */
+public class Exercicio01 {
+    public static void main(String[] args) {
+        
+        /*
+        [DESAFIO]  Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
+         Ex:
+         Qual é o seu nome? João da Silva
+         Olá João da Silva, é um prazer te conhecer!     
+        */
+        
+        
+    }
+}
