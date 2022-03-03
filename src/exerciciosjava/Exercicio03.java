fb9a9b65c471daf75949b8812da48b6cd9f41280
Edinar Nogueira <edinaredms@gmail.com>
2022-03-03 10:53:07 -0300

    Resolvendo exercicio03

--- src/exerciciosjava/Exercicio03.java
+++ src/exerciciosjava/Exercicio03.java
@@ -5,6 +5,8 @@
  */
 package exerciciosjava;
 
+import java.util.Scanner;
+
 /**
  *
  * @author User
@@ -25,6 +27,17 @@ public class Exercicio03 {
          Obs. podem apresentar os dados da forma que acharem melhor.
         
         */
+         Scanner teclado = new Scanner(System.in);
+          
+         System.out.println("INFORME O PRIMEIRO VALOR");
+         int valor1 = teclado.nextInt();
+         System.out.println("INFORM O SEGUNDO VALOR");
+         int valor2 = teclado.nextInt();
+        
+         System.out.println("A SOMA DOS VALORES É DE " + (valor1 + valor2));
+         System.out.println("A DIFERENÇA DOS VALORES É DE " + (valor1 - valor2));
+         System.out.println("A MULTIPLICAÇÃO DOS VALORES É DE " + (valor1 * valor2));
+         System.out.println("A DIVISÃO DOS VALORES É DE " + (valor1 / valor2));
     }
     
 }
