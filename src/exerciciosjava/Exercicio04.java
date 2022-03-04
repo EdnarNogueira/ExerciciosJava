68cf5b2eb2b9f4ef08dcc5bf5653f0970fa3762e
Edinar Nogueira <edinaredms@gmail.com>
2022-03-04 14:25:09 -0300

    Adicionado o exercicio 04

---
+++ src/exerciciosjava/Exercicio04.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosjava;
+
+/**
+ *
+ * @author User
+ */
+public class Exercicio04 {
+    public static void main(String[] args) {
+        
+        /*
+        CRIE UM VETOR PARA ARMAZENAR 4 NOTAS DE 4 ALUNOS E CALCULE A MEDIA DAS NOTAS
+        
+        */
+        
+    }
+}
