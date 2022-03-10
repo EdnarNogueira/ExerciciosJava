557ede9470ebf8b275b6c315a23b08f128937e7c
Edinar Nogueira <edinaredms@gmail.com>
2022-03-09 23:29:32 -0300

    Adicionando exercicio 05

--- src/exerciciosjava/Exercicio04.java
+++ src/exerciciosjava/Exercicio04.java
@@ -12,41 +12,36 @@ import java.util.Scanner;
  * @author User
  */
 public class Exercicio04 {
-    public static void main(String[] args) {
-        
-        /*
-        CRIE UM VETOR PARA ARMAZENAR 4 NOTAS DE 4 ALUNOS E 
-        CALCULE A MEDIA DAS NOTAS        
-        */
-        
-        Scanner teclado = new Scanner(System.in);
+     public static void main(String[] args) {
         
         // Coletando dados do usuário
-        
-        float Vet[] = new float[4];
-        String alunos[] = new String[4];
-        float media = 0;
-        float soma = 0;
-        
-        for(int i = 0; i <= Vet.length ; i++){
-            System.out.println("Informe o nome do " + (i+1)+"º Aluno");
-            alunos[i] = teclado.nextLine();
-            for(int j = 0;  j <= alunos.length -1; j++){
-               System.out.println("Informe a " + (j+1) + "º Nota do " + (i+1)+"º Aluno");
-               Vet[i] = teclado.nextFloat();
-               soma = soma + Vet[i];              
-               media = soma / 4;
-                
-           }//Mostrando os dados na tela
-            teclado.nextLine();
-            System.out.println("TOTAL DAS NOTAS" + soma);
-            System.out.println("A MÉDIA DO " + (i+1) + " º Aluno é " + media);
+         /*
+        CRIE UM VETOR PARA ARMAZENAR 4 NOTAS DE 4 ALUNOS E
+        CALCULE A MEDIA DAS NOTAS
+        */
+       Scanner teclado = new Scanner(System.in);
+       
+       String nome[] = new String[4];
+       float notas[] = new float[4];
+       int i , j , l ;
+       
+              
+       for(i = 0; i <= nome.length; i++){           
+        System.out.println("DIGITE OS NOMES");
+        nome[i] = teclado.nextLine();
+        for(j = 0; j <= nome.length;j++){
+            System.out.println(".,.,.,.,.,.,.,.,.");
+            System.out.println("J " + j);
+            System.out.println("I" + i);
         }
-        
-        
-      
-        
-        
-        
-    }
+       }
+       
+         
+       
+         
+         
 }
+
+}
+    
+   
