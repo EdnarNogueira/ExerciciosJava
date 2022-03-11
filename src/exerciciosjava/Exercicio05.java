8b1687f940935d49504a3261c8fd9be478f836ee
Edinar Nogueira <edinaredms@gmail.com>
2022-03-11 14:19:18 -0300

    Resolvendo exercicio 05

--- src/exerciciosjava/Exercicio05.java
+++ src/exerciciosjava/Exercicio05.java
@@ -1,8 +1,33 @@
 package exerciciosjava;
+
+import java.util.Scanner;
+
 public class Exercicio05 {
-    /*
+    
+    public static void main(String[] args) {
+        /*
      Crie um programa que imprima a tabuada do numero informado pelo
     usuário e a quantidade de calculos       
     */
     
-}
+    /* 
+          RESPOSTA DO EXERCÍCIO 
+        
+       */
+        
+       Scanner teclado = new Scanner(System.in);
+       
+        System.out.println("INFORME UM NUMERO PARA CALCULAR A TABUADA ");
+        int n1 = teclado.nextInt();
+        System.out.println("INFORME A QUANTIDADE DE NUMEROS VOCE DESEJA CALCULAR");
+        int n2 = teclado.nextInt();
+        
+        for(int i = 0; i <= n2; i++){
+            System.out.println(n1 +"*"+ i +" = " + (n1 * i)); 
+        }
+        
+        
+    }
+    
+    
+}    
